bdb2eff3bd912fa8faad45cb89ba78450f385279
ras0q <ras0q@users.noreply.github.com>
2023-11-21 01:13:55 +0900

    :recycle: simplify params

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -66,12 +66,10 @@ func (h *Handler) CreateUser(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, fmt.Errorf("invalid request body: %w", err)).SetInternal(err)
 	}
 
-	params := repository.CreateUserParams{
+	userID, err := h.repo.CreateUser(c.Request().Context(), repository.CreateUserParams{
 		Name:  req.Name,
 		Email: req.Email,
-	}
-
-	userID, err := h.repo.CreateUser(c.Request().Context(), params)
+	})
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError).SetInternal(err)
 	}
